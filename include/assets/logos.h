885a408e9f2dd1b8e186e69bfe4836bb5d1e0f24
Bassel Shmali <bass3l@live.com>
2026-01-15 16:35:59 +0100

    add assets/logos.h

---
+++ include/assets/logos.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "alignment.h"
+
+static const ALIGN_ASSET(2) char LOGO_1[] = "__OTR__logos/LOGO_1";
+
+static const ALIGN_ASSET(2) char LOGO_2[] = "__OTR__logos/LOGO_2";
+
+static const ALIGN_ASSET(2) char LOGO_3[] = "__OTR__logos/LOGO_3";
