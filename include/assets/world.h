27893cbb1c49f3888670a24ef8633e459399e081
Bassel Shmali <bass3l@live.com>
2026-03-04 22:55:21 +0100

    fix enable_status_debuff declaration; update world assets path

--- include/assets/world.h
+++ include/assets/world.h
@@ -2,129 +2,129 @@
 #include "alignment.h"
 
 // --- dgb_01 images ---
-static const ALIGN_ASSET(2) char dgb_01_img0_png[] = "__OTR__world/area_dgb/dgb_01/img0";
-static const ALIGN_ASSET(2) char dgb_01_img1_png[] = "__OTR__world/area_dgb/dgb_01/img1";
-static const ALIGN_ASSET(2) char dgb_01_img2_png[] = "__OTR__world/area_dgb/dgb_01/img2";
-static const ALIGN_ASSET(2) char dgb_01_img3_png[] = "__OTR__world/area_dgb/dgb_01/img3";
-static const ALIGN_ASSET(2) char dgb_01_img4_png[] = "__OTR__world/area_dgb/dgb_01/img4";
-static const ALIGN_ASSET(2) char dgb_01_img5_png[] = "__OTR__world/area_dgb/dgb_01/img5";
-static const ALIGN_ASSET(2) char dgb_01_img6_png[] = "__OTR__world/area_dgb/dgb_01/img6";
-static const ALIGN_ASSET(2) char dgb_01_img7_png[] = "__OTR__world/area_dgb/dgb_01/img7";
+static const ALIGN_ASSET(2) char dgb_01_img0_png[] = "__OTR__world/world/area_dgb/dgb_01/img0";
+static const ALIGN_ASSET(2) char dgb_01_img1_png[] = "__OTR__world/world/area_dgb/dgb_01/img1";
+static const ALIGN_ASSET(2) char dgb_01_img2_png[] = "__OTR__world/world/area_dgb/dgb_01/img2";
+static const ALIGN_ASSET(2) char dgb_01_img3_png[] = "__OTR__world/world/area_dgb/dgb_01/img3";
+static const ALIGN_ASSET(2) char dgb_01_img4_png[] = "__OTR__world/world/area_dgb/dgb_01/img4";
+static const ALIGN_ASSET(2) char dgb_01_img5_png[] = "__OTR__world/world/area_dgb/dgb_01/img5";
+static const ALIGN_ASSET(2) char dgb_01_img6_png[] = "__OTR__world/world/area_dgb/dgb_01/img6";
+static const ALIGN_ASSET(2) char dgb_01_img7_png[] = "__OTR__world/world/area_dgb/dgb_01/img7";
 
 // --- iwa_01 ---
-static const ALIGN_ASSET(2) char iwa_01_splash_img[] = "__OTR__world/area_iwa/iwa_01/splash";
-static const ALIGN_ASSET(2) char iwa_01_splash_gfx[] = "__OTR__world/area_iwa/iwa_01/splash.gfx";
+static const ALIGN_ASSET(2) char iwa_01_splash_img[] = "__OTR__world/world/area_iwa/iwa_01/splash";
+static const ALIGN_ASSET(2) char iwa_01_splash_gfx[] = "__OTR__world/world/area_iwa/iwa_01/splash.gfx";
 
 // --- kmr_02 ---
-static const ALIGN_ASSET(2) char kmr_02_heart_block_img[] = "__OTR__world/area_kmr/kmr_02/heart_block";
-static const ALIGN_ASSET(2) char kmr_02_heart_block_pal[] = "__OTR__world/area_kmr/kmr_02/heart_block.pal";
+static const ALIGN_ASSET(2) char kmr_02_heart_block_img[] = "__OTR__world/world/area_kmr/kmr_02/heart_block";
+static const ALIGN_ASSET(2) char kmr_02_heart_block_pal[] = "__OTR__world/world/area_kmr/kmr_02/heart_block.pal";
 
 // --- kmr_04 ---
-static const ALIGN_ASSET(2) char kmr_04_hammer_block_message_img[] = "__OTR__world/area_kmr/kmr_04/hammer_block_message";
-static const ALIGN_ASSET(2) char kmr_04_hammer_block_message_pal[] = "__OTR__world/area_kmr/kmr_04/hammer_block_message.pal";
+static const ALIGN_ASSET(2) char kmr_04_hammer_block_message_img[] = "__OTR__world/world/area_kmr/kmr_04/hammer_block_message";
+static const ALIGN_ASSET(2) char kmr_04_hammer_block_message_pal[] = "__OTR__world/world/area_kmr/kmr_04/hammer_block_message.pal";
 
 // --- kmr_20 ---
-static const ALIGN_ASSET(2) char kmr_20_records_screen_gfx[] = "__OTR__world/area_kmr/kmr_20/records_screen";
+static const ALIGN_ASSET(2) char kmr_20_records_screen_gfx[] = "__OTR__world/world/area_kmr/kmr_20/records_screen";
 
 // --- kmr_22 ---
-static const ALIGN_ASSET(2) char kmr_22_lava_piranha_vine_img[] = "__OTR__world/area_kmr/kmr_22/lava_piranha_vine";
-static const ALIGN_ASSET(2) char kmr_22_lava_piranha_vine_pal[] = "__OTR__world/area_kmr/kmr_22/lava_piranha_vine.pal";
-static const ALIGN_ASSET(2) char kmr_22_lava_piranha_vine_gfx[] = "__OTR__world/area_kmr/kmr_22/lava_piranha_vine.gfx";
+static const ALIGN_ASSET(2) char kmr_22_lava_piranha_vine_img[] = "__OTR__world/world/area_kmr/kmr_22/lava_piranha_vine";
+static const ALIGN_ASSET(2) char kmr_22_lava_piranha_vine_pal[] = "__OTR__world/world/area_kmr/kmr_22/lava_piranha_vine.pal";
+static const ALIGN_ASSET(2) char kmr_22_lava_piranha_vine_gfx[] = "__OTR__world/world/area_kmr/kmr_22/lava_piranha_vine.gfx";
 #define D_80241900 kmr_22_lava_piranha_vine_img
 #define D_80241D00 kmr_22_lava_piranha_vine_pal
 
 // --- kmr_23 ---
-static const ALIGN_ASSET(2) char kmr_23_window_ul_img[] = "__OTR__world/area_kmr/kmr_23/window_ul";
-static const ALIGN_ASSET(2) char kmr_23_window_ur_img[] = "__OTR__world/area_kmr/kmr_23/window_ur";
-static const ALIGN_ASSET(2) char kmr_23_window_ll_img[] = "__OTR__world/area_kmr/kmr_23/window_ll";
-static const ALIGN_ASSET(2) char kmr_23_window_lr_img[] = "__OTR__world/area_kmr/kmr_23/window_lr";
+static const ALIGN_ASSET(2) char kmr_23_window_ul_img[] = "__OTR__world/world/area_kmr/kmr_23/window_ul";
+static const ALIGN_ASSET(2) char kmr_23_window_ur_img[] = "__OTR__world/world/area_kmr/kmr_23/window_ur";
+static const ALIGN_ASSET(2) char kmr_23_window_ll_img[] = "__OTR__world/world/area_kmr/kmr_23/window_ll";
+static const ALIGN_ASSET(2) char kmr_23_window_lr_img[] = "__OTR__world/world/area_kmr/kmr_23/window_lr";
 
 // --- kzn_19 lava_piranha vine ---
-static const ALIGN_ASSET(2) char kzn_19_lava_piranha_vine_img[] = "__OTR__world/lava_piranha/vine";
-static const ALIGN_ASSET(2) char kzn_19_lava_piranha_vine_pal[] = "__OTR__world/lava_piranha/vine.pal";
-static const ALIGN_ASSET(2) char kzn_19_lava_piranha_vine_gfx[] = "__OTR__world/lava_piranha/vine.gfx";
+static const ALIGN_ASSET(2) char kzn_19_lava_piranha_vine_img[] = "__OTR__world/world/lava_piranha/vine";
+static const ALIGN_ASSET(2) char kzn_19_lava_piranha_vine_pal[] = "__OTR__world/world/lava_piranha/vine.pal";
+static const ALIGN_ASSET(2) char kzn_19_lava_piranha_vine_gfx[] = "__OTR__world/world/lava_piranha/vine.gfx";
 
 // --- mgm_01 ---
-static const ALIGN_ASSET(2) char mgm_01_panel_1_coin_img[] = "__OTR__world/area_mgm/mgm_01/panel_1_coin";
-static const ALIGN_ASSET(2) char mgm_01_panel_1_coin_pal[] = "__OTR__world/area_mgm/mgm_01/panel_1_coin.pal";
-static const ALIGN_ASSET(2) char mgm_01_panel_5_coins_img[] = "__OTR__world/area_mgm/mgm_01/panel_5_coins";
-static const ALIGN_ASSET(2) char mgm_01_panel_5_coins_pal[] = "__OTR__world/area_mgm/mgm_01/panel_5_coins.pal";
-static const ALIGN_ASSET(2) char mgm_01_panel_times_5_img[] = "__OTR__world/area_mgm/mgm_01/panel_times_5";
-static const ALIGN_ASSET(2) char mgm_01_panel_times_5_pal[] = "__OTR__world/area_mgm/mgm_01/panel_times_5.pal";
-static const ALIGN_ASSET(2) char mgm_01_panel_bowser_img[] = "__OTR__world/area_mgm/mgm_01/panel_bowser";
-static const ALIGN_ASSET(2) char mgm_01_panel_bowser_pal[] = "__OTR__world/area_mgm/mgm_01/panel_bowser.pal";
+static const ALIGN_ASSET(2) char mgm_01_panel_1_coin_img[] = "__OTR__world/world/area_mgm/mgm_01/panel_1_coin";
+static const ALIGN_ASSET(2) char mgm_01_panel_1_coin_pal[] = "__OTR__world/world/area_mgm/mgm_01/panel_1_coin.pal";
+static const ALIGN_ASSET(2) char mgm_01_panel_5_coins_img[] = "__OTR__world/world/area_mgm/mgm_01/panel_5_coins";
+static const ALIGN_ASSET(2) char mgm_01_panel_5_coins_pal[] = "__OTR__world/world/area_mgm/mgm_01/panel_5_coins.pal";
+static const ALIGN_ASSET(2) char mgm_01_panel_times_5_img[] = "__OTR__world/world/area_mgm/mgm_01/panel_times_5";
+static const ALIGN_ASSET(2) char mgm_01_panel_times_5_pal[] = "__OTR__world/world/area_mgm/mgm_01/panel_times_5.pal";
+static const ALIGN_ASSET(2) char mgm_01_panel_bowser_img[] = "__OTR__world/world/area_mgm/mgm_01/panel_bowser";
+static const ALIGN_ASSET(2) char mgm_01_panel_bowser_pal[] = "__OTR__world/world/area_mgm/mgm_01/panel_bowser.pal";
 
 // --- mgm_02 ---
-static const ALIGN_ASSET(2) char mgm_02_panel_peach_img[] = "__OTR__world/area_mgm/mgm_02/panel_peach";
-static const ALIGN_ASSET(2) char mgm_02_panel_peach_pal[] = "__OTR__world/area_mgm/mgm_02/panel_peach.pal";
+static const ALIGN_ASSET(2) char mgm_02_panel_peach_img[] = "__OTR__world/world/area_mgm/mgm_02/panel_peach";
+static const ALIGN_ASSET(2) char mgm_02_panel_peach_pal[] = "__OTR__world/world/area_mgm/mgm_02/panel_peach.pal";
 
 // --- dro_02 ---
-static const ALIGN_ASSET(2) char dro_02_card_img[] = "__OTR__world/area_dro/dro_02/card";
-static const ALIGN_ASSET(2) char dro_02_card_pal[] = "__OTR__world/area_dro/dro_02/card.pal";
-static const ALIGN_ASSET(2) char dro_02_card_setup_gfx[] = "__OTR__world/area_dro/dro_02/card_setup.gfx";
-static const ALIGN_ASSET(2) char dro_02_card_1_gfx[] = "__OTR__world/area_dro/dro_02/card_1.gfx";
-static const ALIGN_ASSET(2) char dro_02_card_2_gfx[] = "__OTR__world/area_dro/dro_02/card_2.gfx";
+static const ALIGN_ASSET(2) char dro_02_card_img[] = "__OTR__world/world/area_dro/dro_02/card";
+static const ALIGN_ASSET(2) char dro_02_card_pal[] = "__OTR__world/world/area_dro/dro_02/card.pal";
+static const ALIGN_ASSET(2) char dro_02_card_setup_gfx[] = "__OTR__world/world/area_dro/dro_02/card_setup.gfx";
+static const ALIGN_ASSET(2) char dro_02_card_1_gfx[] = "__OTR__world/world/area_dro/dro_02/card_1.gfx";
+static const ALIGN_ASSET(2) char dro_02_card_2_gfx[] = "__OTR__world/world/area_dro/dro_02/card_2.gfx";
 #define dro_02_card dro_02_card_img
 
 // --- pra_31 ---
-static const ALIGN_ASSET(2) char pra_31_texture_1_img[] = "__OTR__world/area_pra/pra_31/texture_1";
-static const ALIGN_ASSET(2) char pra_31_texture_1_pal[] = "__OTR__world/area_pra/pra_31/texture_1.pal";
-static const ALIGN_ASSET(2) char pra_31_texture_1_gfx[] = "__OTR__world/area_pra/pra_31/texture_1.gfx";
-static const ALIGN_ASSET(2) char pra_31_texture_2_img[] = "__OTR__world/area_pra/pra_31/texture_2";
-static const ALIGN_ASSET(2) char pra_31_texture_2_pal[] = "__OTR__world/area_pra/pra_31/texture_2.pal";
-static const ALIGN_ASSET(2) char pra_31_texture_2_gfx[] = "__OTR__world/area_pra/pra_31/texture_2.gfx";
-static const ALIGN_ASSET(2) char pra_31_texture_3_img[] = "__OTR__world/area_pra/pra_31/texture_3";
-static const ALIGN_ASSET(2) char pra_31_texture_3_gfx[] = "__OTR__world/area_pra/pra_31/texture_3.gfx";
+static const ALIGN_ASSET(2) char pra_31_texture_1_img[] = "__OTR__world/world/area_pra/pra_31/texture_1";
+static const ALIGN_ASSET(2) char pra_31_texture_1_pal[] = "__OTR__world/world/area_pra/pra_31/texture_1.pal";
+static const ALIGN_ASSET(2) char pra_31_texture_1_gfx[] = "__OTR__world/world/area_pra/pra_31/texture_1.gfx";
+static const ALIGN_ASSET(2) char pra_31_texture_2_img[] = "__OTR__world/world/area_pra/pra_31/texture_2";
+static const ALIGN_ASSET(2) char pra_31_texture_2_pal[] = "__OTR__world/world/area_pra/pra_31/texture_2.pal";
+static const ALIGN_ASSET(2) char pra_31_texture_2_gfx[] = "__OTR__world/world/area_pra/pra_31/texture_2.gfx";
+static const ALIGN_ASSET(2) char pra_31_texture_3_img[] = "__OTR__world/world/area_pra/pra_31/texture_3";
+static const ALIGN_ASSET(2) char pra_31_texture_3_gfx[] = "__OTR__world/world/area_pra/pra_31/texture_3.gfx";
 // Stairs skeleton DLs
-static const ALIGN_ASSET(2) char pra_31_unk_01_gfx[] = "__OTR__world/area_pra/pra_31/unk_01.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_02_gfx[] = "__OTR__world/area_pra/pra_31/unk_02.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_03_gfx[] = "__OTR__world/area_pra/pra_31/unk_03.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_04_gfx[] = "__OTR__world/area_pra/pra_31/unk_04.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_05_gfx[] = "__OTR__world/area_pra/pra_31/unk_05.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_06_gfx[] = "__OTR__world/area_pra/pra_31/unk_06.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_07_gfx[] = "__OTR__world/area_pra/pra_31/unk_07.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_08_gfx[] = "__OTR__world/area_pra/pra_31/unk_08.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_09_gfx[] = "__OTR__world/area_pra/pra_31/unk_09.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_10_gfx[] = "__OTR__world/area_pra/pra_31/unk_10.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_11_gfx[] = "__OTR__world/area_pra/pra_31/unk_11.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_12_gfx[] = "__OTR__world/area_pra/pra_31/unk_12.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_13_gfx[] = "__OTR__world/area_pra/pra_31/unk_13.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_14_gfx[] = "__OTR__world/area_pra/pra_31/unk_14.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_15_gfx[] = "__OTR__world/area_pra/pra_31/unk_15.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_16_gfx[] = "__OTR__world/area_pra/pra_31/unk_16.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_17_gfx[] = "__OTR__world/area_pra/pra_31/unk_17.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_18_gfx[] = "__OTR__world/area_pra/pra_31/unk_18.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_19_gfx[] = "__OTR__world/area_pra/pra_31/unk_19.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_20_gfx[] = "__OTR__world/area_pra/pra_31/unk_20.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_21_gfx[] = "__OTR__world/area_pra/pra_31/unk_21.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_22_gfx[] = "__OTR__world/area_pra/pra_31/unk_22.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_23_gfx[] = "__OTR__world/area_pra/pra_31/unk_23.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_24_gfx[] = "__OTR__world/area_pra/pra_31/unk_24.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_25_gfx[] = "__OTR__world/area_pra/pra_31/unk_25.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_26_gfx[] = "__OTR__world/area_pra/pra_31/unk_26.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_27_gfx[] = "__OTR__world/area_pra/pra_31/unk_27.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_28_gfx[] = "__OTR__world/area_pra/pra_31/unk_28.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_29_gfx[] = "__OTR__world/area_pra/pra_31/unk_29.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_30_gfx[] = "__OTR__world/area_pra/pra_31/unk_30.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_31_gfx[] = "__OTR__world/area_pra/pra_31/unk_31.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_32_gfx[] = "__OTR__world/area_pra/pra_31/unk_32.gfx";
-static const ALIGN_ASSET(2) char pra_31_unk_33_gfx[] = "__OTR__world/area_pra/pra_31/unk_33.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_01_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_01.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_02_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_02.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_03_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_03.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_04_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_04.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_05_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_05.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_06_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_06.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_07_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_07.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_08_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_08.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_09_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_09.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_10_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_10.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_11_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_11.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_12_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_12.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_13_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_13.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_14_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_14.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_15_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_15.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_16_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_16.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_17_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_17.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_18_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_18.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_19_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_19.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_20_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_20.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_21_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_21.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_22_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_22.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_23_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_23.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_24_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_24.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_25_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_25.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_26_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_26.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_27_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_27.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_28_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_28.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_29_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_29.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_30_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_30.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_31_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_31.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_32_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_32.gfx";
+static const ALIGN_ASSET(2) char pra_31_unk_33_gfx[] = "__OTR__world/world/area_pra/pra_31/unk_33.gfx";
 
 // --- sam_05 ---
-static const ALIGN_ASSET(2) char sam_05_monstar_png[] = "__OTR__world/area_sam/sam_05/monstar";
-static const ALIGN_ASSET(2) char sam_05_monstar_pal[] = "__OTR__world/area_sam/sam_05/monstar.pal";
-static const ALIGN_ASSET(2) char sam_05_monstar_blank1_png[] = "__OTR__world/area_sam/sam_05/monstar_blank1";
-static const ALIGN_ASSET(2) char sam_05_monstar_blank2_png[] = "__OTR__world/area_sam/sam_05/monstar_blank2";
-static const ALIGN_ASSET(2) char sam_05_monstar_blank3_png[] = "__OTR__world/area_sam/sam_05/monstar_blank3";
-static const ALIGN_ASSET(2) char sam_05_monstar_gfx[] = "__OTR__world/area_sam/sam_05/monstar.gfx";
+static const ALIGN_ASSET(2) char sam_05_monstar_png[] = "__OTR__world/world/area_sam/sam_05/monstar";
+static const ALIGN_ASSET(2) char sam_05_monstar_pal[] = "__OTR__world/world/area_sam/sam_05/monstar.pal";
+static const ALIGN_ASSET(2) char sam_05_monstar_blank1_png[] = "__OTR__world/world/area_sam/sam_05/monstar_blank1";
+static const ALIGN_ASSET(2) char sam_05_monstar_blank2_png[] = "__OTR__world/world/area_sam/sam_05/monstar_blank2";
+static const ALIGN_ASSET(2) char sam_05_monstar_blank3_png[] = "__OTR__world/world/area_sam/sam_05/monstar_blank3";
+static const ALIGN_ASSET(2) char sam_05_monstar_gfx[] = "__OTR__world/world/area_sam/sam_05/monstar.gfx";
 #define D_80242970 sam_05_monstar_blank1_png
 #define D_80242B70 sam_05_monstar_blank2_png
 #define D_80242D70 sam_05_monstar_blank3_png
 
 // --- tst_13 ---
-static const ALIGN_ASSET(2) char tst_13_shockwave_gfx[] = "__OTR__world/area_tst/tst_13/shockwave.gfx";
+static const ALIGN_ASSET(2) char tst_13_shockwave_gfx[] = "__OTR__world/world/area_tst/tst_13/shockwave.gfx";
 
 // --- toad_house ---
-static const ALIGN_ASSET(2) char toad_house_blanket_img[] = "__OTR__world/toad_house/blanket";
-static const ALIGN_ASSET(2) char toad_house_blanket_gfx[] = "__OTR__world/toad_house/blanket.gfx";
-static const ALIGN_ASSET(2) char toad_house_blanket_vtx[] = "__OTR__world/toad_house/blanket.vtx";
+static const ALIGN_ASSET(2) char toad_house_blanket_img[] = "__OTR__world/world/toad_house/blanket";
+static const ALIGN_ASSET(2) char toad_house_blanket_gfx[] = "__OTR__world/world/toad_house/blanket.gfx";
+static const ALIGN_ASSET(2) char toad_house_blanket_vtx[] = "__OTR__world/world/toad_house/blanket.vtx";
