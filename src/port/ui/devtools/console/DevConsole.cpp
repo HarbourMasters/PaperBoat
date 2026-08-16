106861ac34c82556b320a4dbeb6922a676c99447
Bassel Shmali <bass3l@live.com>
2026-08-16 12:01:16 +0200

    wire the reset button

--- src/port/ui/devtools/console/DevConsole.cpp
+++ src/port/ui/devtools/console/DevConsole.cpp
@@ -3,6 +3,8 @@
 #include <ship/Context.h>
 #include <ship/window/Window.h>
 
+#include "game_modes.h"
+
 #define CMD_REGISTER Ship::Context::GetInstance()->GetConsole()->AddCommand
 // TODO: Commands should be using the output passed in.
 #define ERROR_MESSAGE                                                          \
@@ -16,10 +18,14 @@
           "Console"))                                                          \
       ->SendInfoMessage
 
+// Defined in state_startup.c.
+extern "C" b32 gPortResetToTitleScreen;
+
 static bool ResetHandler(std::shared_ptr<Ship::Console> Console,
                          std::vector<std::string> args, std::string *output) {
-  // @port: Todo
-  return 0;
+  gPortResetToTitleScreen = true;
+  gOverrideFlags |= GLOBAL_OVERRIDES_SOFT_RESET;
+  return 1;
 }
 
 void DevConsole_Init(void) {
